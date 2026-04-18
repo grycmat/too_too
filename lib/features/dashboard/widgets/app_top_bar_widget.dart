44f2c724a38e668c6244fdf16d5ca5120221d84a
grycmat <mateusz@gryc.dev>
2026-04-18 14:10:40 +0200

    minor visual improvements

--- lib/features/dashboard/widgets/app_top_bar_widget.dart
+++ lib/features/dashboard/widgets/app_top_bar_widget.dart
@@ -10,25 +10,22 @@ class AppTopBarWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(20, 0, 20, 4),
-      child: ClipRRect(
-        child: Padding(
-          padding: const EdgeInsets.only(top: 40),
-          child: BackdropFilter(
-            filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(title, style: Theme.of(context).textTheme.headlineLarge),
-                IconButton(
-                  onPressed: () {
-                    context.pushNamed('/settings');
-                  },
-                  icon: Icon(Icons.settings_rounded),
-                ),
-              ],
-            ),
+    return ClipRRect(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 40, left: 20, right: 20),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(title, style: Theme.of(context).textTheme.headlineLarge),
+              IconButton(
+                onPressed: () {
+                  context.pushNamed('/settings');
+                },
+                icon: Icon(Icons.settings_rounded),
+              ),
+            ],
           ),
         ),
       ),
